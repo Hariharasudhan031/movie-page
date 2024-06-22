385790837fd59f3cc28453e5d8c43f9286c32d5c
Hariharasudhan031 <imharish3198@gmail.com>
2024-06-22 09:18:26 +0000

    solution

---
+++ src/components/Upcoming/index.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Loader from 'react-loader-spinner'
+
+import MovieCard from '../MovieCard'
+import NavBar from '../NavBar'
+import Pagination from '../Pagination'
+
+import './index.css'
+
+class Upcoming extends React.Component {
+  state = {
+    isLoading: true,
+    upcomingMovieResponse: {},
+  }
+
+  componentDidMount() {
+    this.getUpcomingMoviesResponse()
+  }
+
+  getUpdatedData = responseData => ({
+    totalPages: responseData.total_pages,
+    totalResults: responseData.total_results,
+    results: responseData.results.map(eachMovie => ({
+      id: eachMovie.id,
+      posterPath: `https://image.tmdb.org/t/p/w500${eachMovie.poster_path}`,
+      voteAverage: eachMovie.vote_average,
+      title: eachMovie.title,
+    })),
+  })
+
+  getUpcomingMoviesResponse = async (page = 1) => {
+    const API_KEY = 'f78f71277dmsh1c9b7d33f0ae3e1p180864jsnb3489b4f0af8'
+    const apiUrl = `https://moviesminidatabase.p.rapidapi.com/movie/upcoming?api_key=${API_KEY}&language=en-US&page=${page}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    const newData = this.getUpdatedData(data)
+    this.setState({isLoading: false, upcomingMovieResponse: newData})
+  }
+
+  renderLoadingView = () => (
+    <div className="loader-container">
+      <Loader type="TailSpin" color="#032541" />
+    </div>
+  )
+
+  renderPopularMoviesList = () => {
+    const {upcomingMovieResponse} = this.state
+    const {results} = upcomingMovieResponse
+
+    return (
+      <ul className="row p-0 ms-0 me-0 mt-3">
+        {results.map(movie => (
+          <MovieCard key={movie.id} movieDetails={movie} />
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    const {isLoading, upcomingMovieResponse} = this.state
+
+    return (
+      <>
+        <NavBar />
+        <div className="route-page-body">
+          {isLoading
+            ? this.renderLoadingView()
+            : this.renderPopularMoviesList()}
+        </div>
+        <Pagination
+          totalPages={upcomingMovieResponse.totalPages}
+          apiCallback={this.getUpcomingMoviesResponse}
+        />
+      </>
+    )
+  }
+}
+
+export default Upcoming
